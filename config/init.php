88bbc55ae8dbe766b03e015d3485de50e87820c3
Bulavkin Nikolay <bulnikn@gmail.com>
2019-10-23 16:21:05 +0300

    Complite init
    		#

--- config/init.php
+++ config/init.php
@@ -1,2 +1,24 @@
 <?php
-echo "init <br/>";
+define ("DEBUG",1);
+define  ("ROOT",dirname(__DIR__));
+define ("WWW",ROOT.'/public');
+define ("APP",ROOT.'/app');
+define ("CORE", ROOT.'/vendor/ishop/core');
+define ("LIBS", CORE.'/libs');
+define ("CACHE", ROOT.'/temp/cache');
+define ("CONF", ROOT.'/config');
+define ("LAYOUT",'default');
+
+// http://localhost:81/ishop2.loc/public/index.php
+ $app_path = "http://{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}";
+// http://localhost:81/ishop2.loc/public/
+ $app_path = preg_replace("#[^/]+$#",'',$app_path);
+// http://localhost:81/ishop2.loc
+ $app_path = str_replace("/public/",'',$app_path);
+
+define ("PATH",$app_path);
+define ("ADMIN",PATH.'/admin');
+
+require_once ROOT.'/vendor/autoload.php';
+
+
--- public/index.php
+++ public/index.php
@@ -1,5 +1,8 @@
 <?php
 require_once dirname(__DIR__).'/config/init.php';
-echo 'show must go on! Го';
+
 var_dump($_SERVER['QUERY_STRING']);
+
+echo "host =",PATH;
+
 
